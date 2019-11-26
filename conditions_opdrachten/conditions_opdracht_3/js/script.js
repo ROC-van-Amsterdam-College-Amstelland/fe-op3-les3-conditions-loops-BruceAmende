bd4f5ecab039a96518fef5a55a74a435e8684345
BruceAmende <bruceamende32@gmail.com>
2019-11-26 12:39:20 +0100

    opdr3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,19 @@
 //javascript code
+
+function checkSaldo() {
+
+    var banksaldo = document.getElementById("geldOpname").value;
+
+
+    if(banksaldo == 500){
+        alert("Er is gestort!");
+    }
+
+    if(banksaldo > 500){
+        alert("Bonus is gestort!");
+
+        
+    }
+
+ 
+}
