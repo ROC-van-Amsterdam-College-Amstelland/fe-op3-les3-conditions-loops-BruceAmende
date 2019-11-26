64b8e187198678af21a83fd53d241746fda001c1
BruceAmende <bruceamende32@gmail.com>
2019-11-26 11:11:23 +0100

    opdr2

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+
+function checkSaldo() {
+
+    var geldOpname = document.getElementById("geldOpname").value;
+
+    var bankSaldo = 1000;
+
+    var restTotaal = 1000 - geldOpname;
+
+    if(restTotaal < 25){
+        alert("Sorry, je hebt te weinig saldo!");
+    }
+}
