151fee2277ffa5dd5487546ec7c5aab02d8fdbc4
BruceAmende <bruceamende32@gmail.com>
2019-11-27 09:20:19 +0100

    opdr6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,27 @@
 //javascript code
+
+var myOperator = "/";
+var myNumber1 = prompt("Geef een getal");
+var myNumber2 = prompt("Geef nog een getal");
+var resultaat;
+
+
+
+if(myOperator == "*"){
+
+    resultaat = Number (myNumber1) * Number (myNumber2);
+    document.getElementById("tekst").innerHTML = resultaat;
+}
+
+
+
+
+
+else if(myOperator == "/"){
+
+    resultaat = myNumber1 / myNumber2;
+    document.getElementById("tekst").innerHTML = resultaat;
+   
+
+
+}
