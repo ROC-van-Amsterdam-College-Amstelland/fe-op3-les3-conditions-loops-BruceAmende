c7658018257637346312356bcff1e75c61d7c429
BruceAmende <bruceamende32@gmail.com>
2019-12-05 15:18:45 +0100

    loop opdr 7

--- loops_opdrachten/loops_opdracht_6/js/script.js
+++ loops_opdrachten/loops_opdracht_6/js/script.js
@@ -1,1 +1,38 @@
 //javascript code
+
+var maxgetal
+
+function bepaal(params) {
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+for(var teller = 1 ; teller <= 29 ; teller+=1 ){
+
+    console.log(teller);
+
+   document.getElementById("getal").innerHTML += teller + "<br>";
+  
+
+}
+
+for(var teller = 1 ; teller <= 20 ; teller+=1 ){
+
+    
+
+   console.log = (teller);
+  
+
+}
+
+
--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,45 @@
 //javascript code
+
+
+for(var teller = 1 ; teller <= 100 ; teller+=1 ){
+
+    if(teller % 3 == 0 && teller % 5 == 0) {
+        document.getElementById("getal").innerHTML += "<strong>VET COOL</strong><br>";
+
+        
+
+
+
+    }
+
+    
+
+   
+  
+
+    if(teller % 3 == 0){
+
+        document.getElementById("getal").innerHTML += "<strong>VET</strong><br>";
+
+
+    }
+
+   else if(teller % 5 == 0){
+
+        document.getElementById("getal").innerHTML += "<strong>COOL</strong><br>";
+
+
+    }
+
+
+
+
+    else{
+
+        document.getElementById("getal").innerHTML += teller + "<br>";
+
+        
+    }
+    
+
+}
