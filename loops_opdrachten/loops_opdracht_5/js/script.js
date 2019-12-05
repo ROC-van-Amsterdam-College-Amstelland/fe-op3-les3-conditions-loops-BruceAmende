a2b05dc4c183f01b4946be6672e163db08e0ab25
BruceAmende <bruceamende32@gmail.com>
2019-12-05 12:50:04 +0100

    loops opdr 5

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,16 @@
-//javascript code
+for(var tellere = 0 ; tellere <= 20 ; tellere+=2 ){
+
+    
+
+   document.getElementById("getal").innerHTML += tellere + "<br>";
+
+}//javascript code
+
+
+for(var tellero = 0 ; tellero <= 20 ; tellero+=3 ){
+
+    
+
+   console.log(tellero);
+
+}//javasc
