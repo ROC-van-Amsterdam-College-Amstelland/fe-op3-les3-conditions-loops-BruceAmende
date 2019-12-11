d57c9acf06d51992952f5f77786dff89d014d38e
BruceAmende <bruceamende32@gmail.com>
2019-12-11 10:58:45 +0100

    loops opdr9

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,33 @@
 //javascript code
+
+
+for(var teller = 0 ; teller <= 5 ; teller+= 1){
+
+    console.log(teller);
+    
+    
+    if(teller ==5){
+
+        alert("Het einde is bereikt!")
+
+
+
+    }
+
+}
+    
+for(var teller = 4 ; teller >= 0 ; teller --  ){
+
+    console.log(teller);
+    
+    
+    if(teller ==0){
+
+        alert("Het begin is het begin!")
+
+
+
+    }
+
+}
+    
