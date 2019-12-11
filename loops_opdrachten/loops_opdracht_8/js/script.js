8754dce2aef3c0121c79d3c4a82c30c6cbbda8b6
BruceAmende <bruceamende32@gmail.com>
2019-12-11 10:40:47 +0100

    loops opdr8

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,17 @@
 //javascript code
+
+for(var teller = 1 ; teller <= 50 ; teller+= 1){
+
+console.log(teller);
+
+document.getElementById("getal").innerHTML += teller + "<br>";
+
+if(teller == 25){
+
+alert("Het getal 25 is net geen bullseye!");
+
+
+}
+
+}
+
